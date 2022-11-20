3982e3029dc058a7b4a12d3e961dba6e31832013
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-11-20 23:14:11 +0100

    rerun

--- notebooks/_solutions/13-raster-processing48.py
+++ notebooks/_solutions/13-raster-processing48.py
@@ -1,2 +1,1 @@
-%%time
 green_area = focal.focal_stats(green_arr, kernel, stats_funcs=["sum"])
--- notebooks/_solutions/case-argo-sea-floats12.py
+++ notebooks/_solutions/case-argo-sea-floats12.py
@@ -1,5 +1,6 @@
 salinity_20121031 = argo["salinity"].sel(date='2012-10-31')
 
 fig, ax = plt.subplots(figsize=(6, 6))
-salinity_20121031.plot.line(y="level", yincrease=False, color="grey");
-salinity_20121031.rolling(level=10, center=True).median().plot.line(y="level", yincrease=False, linewidth=3, color="orange");
+salinity_20121031.plot.line(y="level", yincrease=False, color="grey", add_legend=False, label="raw");
+salinity_20121031.rolling(level=10, center=True).median().plot.line(y="level", yincrease=False, linewidth=3, color="orange", add_legend=False, label="smooth");
+ax.legend()
